b0528ab1a6c4adb22badc9bd8cbc9dd0b7bf0341
Ruchi <ruchikharwar4@gmail.com>
2019-02-16 15:56:57 +0530

    added style in productpage

--- src/components/modules/ImageSec.js
+++ src/components/modules/ImageSec.js
@@ -5,9 +5,10 @@ class ImageSec extends Component {
     return (
       <div className="image-sec__element">
         <img src="https://picsum.photos/200" />
-        <h4>dffsdfsdf</h4>
-        <p><small>dfsf</small></p>
-        <p>lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+        <h4>Mary Lou</h4>
+        <div><small>codedrop.com</small></div>
+        <p className="paragraph"><small>A freelance web designer and developer with a passion for interaction 
+        design. Co-founder & author of @codrops</small></p>
         <div>
           <span>Twitter</span>
           <span>Twitter</span>
